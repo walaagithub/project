334e48eee372ac1a735d85411a9e1f6c6413f5f1
Walaa Fadil <walaafathel@yahoo.com>
2017-09-30 12:44:11 +0300

    This is my first project

---
+++ hw2/hw2.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>HW2</title>
+
+    <!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+     <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+       <!-- Latest compiled and minified JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  </head>
+    
+  <body>
+      
+      <!-------------------------------------------------------->
+      
+      
+       <div class="jumbotron">
+         <div class="container">
+          <h2>Online Store</h2>      
+          <p> Mission, Vission & Values</p>
+       </div> 
+      </div>
+      
+      
+      <!------------------------------------------------------>
+ 
+      
+    <!----------------------------------navbar header---------------------->
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Project</a></li>
+      <li><a href="#">Contect</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Your Account</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+      
+      
+      <!------------------------------the boxes-------------------------------->
+ <div class="container">
+    <div class ="main">
+        <div class="row">
+             <!---------------first row 1box---------------------------------->
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                        <div class="thumbnail1">
+                            
+                               <div class="hb1"><p>title</p></div>
+                            <a href="" ><img src="http://placehold.it/320x150" alt=""></a>
+                               <div class="db1">
+                                 <p>Buy 50 jjfllfhdj kjkdj</p>   
+                              </div>
+                            
+                         </div>
+             </div>
+            
+            <!--------------------first row 2box------------------------------>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                        <div class="thumbnail2">
+                            
+                               <div class="hb2"><p>title</p></div>
+                            <a href="" ><img src="http://placehold.it/320x150" alt=""></a>
+                               <div class="db2">
+                                 <p>Buy 50 jjfllfhdj kjkdj</p>   
+                              </div>
+                            
+                         </div>
+             </div>
+            <!------------------------------------------------->
+             <!--------------------first row 3box------------------------------>
+             <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                        <div class="thumbnail3">
+                            
+                               <div class="hb3"><p>title</p></div>
+                            <a href="" ><img src="http://placehold.it/320x150" alt=""></a>
+                               <div class="db3">
+                                 <p>Buy 50 jjfllfhdj kjkdj</p>   
+                              </div>
+                           
+                         </div>
+             </div>
+            <!------------------------------------------------->
+        </div><!--row 1-->
+        <div class="row">
+      
+             <!-----------------second row 1box--------------------------------->
+             <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                        <div class="thumbnail4">
+                            
+                               <div class="hb4"><p>title</p></div>
+                            <a href="" ><img src="http://placehold.it/320x150" alt=""></a>
+                               <div class="db4">
+                                 <p>Buy 50 jjfllfhdj kjkdj</p>   
+                              </div>
+                            
+                         </div>
+             </div>
+            <!------------------------------------------------->
+             <!---------------------second row 2box---------------------------->
+             <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                        <div class="thumbnail5">
+                            
+                               <div class="hb5"><p>title</p></div>
+                            <a href="" ><img src="http://placehold.it/320x150" alt=""></a>
+                               <div class="db5">
+                                 <p>Buy 50 jjfllfhdj kjkdj</p>   
+                              </div>
+                           
+                         </div>
+             </div>
+            <!------------------------------------------------->
+             <!-------------------second row 3box------------------------------>
+             <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                        <div class="thumbnail6">
+                            
+                               <div class="hb6"><p>title</p></div>
+                            <a href="" ><img src="http://placehold.it/320x150" alt=""></a>
+                               <div class="db6">
+                                 <p>Buy 50 jjfllfhdj kjkdj</p>   
+                              </div>
+                           
+                         </div>
+             </div>
+            <!------------------------------------------------->
+        </div><!--row 2-->
+     </div><!--main-->
+      </div><!--container-->
+    
+      
+      <!-----------------------------footer--------------------------------->  
+      
+     <footer class="footer">
+      <div class="container">
+        <span class="text-muted"> Oline Store Copyright &copy;</span>
+         <form class="form-inline">
+          <label class="mr-sm-2"  for="inlineFormInput">Get deals: </label>
+            <input type="text" size="50" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Email Address">
+            <button type="submit" class="btn btn-danger">Sign Up</button>
+         </form>
+      </div>
+     </footer> 
+       <!--------------------------------------------------------------> 
+      
+  </body>
+</html>
